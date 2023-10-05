3c43c671ad3a0163970a97828e171c2805b97398
Davide <22103897+dvdblg@users.noreply.github.com>
2023-10-05 15:36:42 +0200

    add option to toggle the comment of .env.template entry

--- src/dotenv_template/__init__.py
+++ src/dotenv_template/__init__.py
@@ -3,9 +3,11 @@ import re
 
 def create_template(
     env_file: str = '.env',
-    env_template_file: str = '.env.template'
+    env_template_file: str = '.env.template',
+    also_comment: bool = False,
 ):
     if os.path.exists(env_file):
+        base_string = '# ' if also_comment else ''
         with open(env_file, 'r') as f:
             env_vars = f.readlines()
         env_template_lines = []
@@ -14,7 +16,7 @@ def create_template(
             m = re.match(r'^\s*(\w+)=(.*)', line)
             if m:
                 # replace the value with a placeholder
-                env_template_lines.append(f'# {m.group(1)}=REPLACE_ME\n')
+                env_template_lines.append(f'{base_string}{m.group(1)}=REPLACE_ME\n')
             else:
                 env_template_lines.append(line)
         with open(env_template_file, 'w') as f:
